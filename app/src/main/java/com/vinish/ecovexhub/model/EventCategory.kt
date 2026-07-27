f9e2dda419622d70ee09e3e8631a71d51e09ecda
Vinish <mm7920460@gmail.com>
2026-07-27 18:09:20 +0530

    feat(event): update event category to support more details

--- app/src/main/java/com/vinish/ecovexhub/model/EventCategory.kt
+++ app/src/main/java/com/vinish/ecovexhub/model/EventCategory.kt
@@ -1,9 +1,15 @@
 package com.vinish.ecovexhub.model
 
-enum class EventCategory{
-    CHALLENGE,
-    WORKSHOP,
-    TALK,
-    DRIVE,
-    COMPETITION
+import androidx.annotation.DrawableRes
+import com.vinish.ecovexhub.R
+
+enum class EventCategory(
+    val displayName: String,
+    @DrawableRes bannerRes: Int
+) {
+    CHALLENGE("Challenge", R.drawable.event_competition),
+    WORKSHOP("Workshop", R.drawable.event_talks),
+    TALK("Talk", R.drawable.event_talks),
+    DRIVE("Drive", R.drawable.event_drive),
+    CREATIVE("Creative",R.drawable.event_creative )
 }
