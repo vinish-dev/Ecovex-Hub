4ae5c1b02da83002440afe07ecd62f19fc217d05
Vinish <mm7920460@gmail.com>
2026-07-27 11:55:05 +0530

    feat(ui): add  SectionHeader.kt
    - Add app colors

---
+++ app/src/main/java/com/vinish/ecovexhub/ui/components/SectionHeader.kt
@@ -0,0 +1,48 @@
+package com.vinish.ecovexhub.ui.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import com.vinish.ecovexhub.theme.ViewAllBlue
+
+@Composable
+fun SectionHeader(
+    title: String,
+    modifier: Modifier = Modifier,
+    showViewAll: Boolean = true, //optional
+    onViewAllClick: () -> Unit = {}, //optional
+) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+        ) {
+        Text(
+            text = "Upcoming",
+            fontWeight = FontWeight.Bold,
+            style = MaterialTheme.typography.titleMedium
+        )
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        if (showViewAll) {
+            TextButton(onClick = onViewAllClick) {
+                Text(
+                    text = "View All",
+                    fontWeight = FontWeight.Bold,
+                    color = ViewAllBlue
+                    )
+            }
+        }
+    }
+}
