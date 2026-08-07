5e2a32e0c7fa22ec9baf84bfc9c3362fc272c41a
Vinish <mm7920460@gmail.com>
2026-08-07 15:07:25 +0530

    feat(EventDetails): create event details tab enum

---
+++ app/src/main/java/com/vinish/ecovexhub/model/EventDetailsTab.kt
@@ -0,0 +1,9 @@
+package com.vinish.ecovexhub.model
+
+enum class EventDetailsTab(val title: String) {
+    Overview("Overview"),
+    Timeline("Timeline"),
+    Gallery("Gallery"),
+    Leaderboard("Leaderboard")
+}
+
